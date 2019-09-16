1ea9407350dbc799fd2acebe7fe919b4977fd9c4
nggrekkodev <nggrekkodev@gmail.com>
2019-09-16 03:48:17 +0200

    clear

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
+// Core modules
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-// Synchronous way
+// Third party modules
+const slugify = require('slugify');
 
-// const text = fs.readFileSync('./txt/input.txt','utf-8');
-// console.log(text);
-// const textOut = `This is what we know about the avocado : ${text}. \nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt',textOut);
-// console.log('File written');
+// Own modules
+const replaceTemplate = require('./modules/replaceTemplate');
 
+// ************************ SERVER
+// fs.readFile('./dev-data/data.json')
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-// Asynchronous way callbacks
-fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
-  if (err) return console.log('error');
-  fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`,'utf-8',(err,data3) => {
-      console.log(data3);
-      fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`,'utf-8',err => {
-        console.log('your file has been written');
-      })
-    });
-  });
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+const slugs = dataObject.map(element => slugify(element.productName, { lower: true }));
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+    // ES6 Destructuring
+    const { query, pathname } = url.parse(req.url, true);
+    // const pathname = req.url;
+
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        // array
+        // const cardsHtml = dataObject.map(element => replaceTemplate(templateCard, element))
+        // join all elements with string
+        const cardsHtml = dataObject.map(element => replaceTemplate(templateCard, element)).join('');
+
+        // replace the placeholder with cards
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(output);
+    }
+
+    // Product page
+    else if (pathname === '/product') {
+        // console.log(query);
+        const product = dataObject[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(output);
+    }
+
+    // API
+    else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+    }
+
+    // Not found
+    else {
+        // send headers always before the response
+        res.writeHead(404, {
+            // headers : piece of information about the response
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
 });
---
+++ modules/replaceTemplate.js
@@ -0,0 +1,34 @@
+// Creates a copy of template-card and replaces each placeholder with the product value
+
+module.exports = (template, product) => {
+  // /{%PRODUCTNAME%}/g : replace all {%PRODUCTNAME%} fields
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
+// const replaceTemplate = (template, product) => {
+//   // /{%PRODUCTNAME%}/g : replace all {%PRODUCTNAME%} fields
+//   let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%PRICE%}/g, product.price);
+//   output = output.replace(/{%FROM%}/g, product.from);
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//   output = output.replace(/{%ID%}/g, product.id);
+
+//   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+//   return output;
+// }
